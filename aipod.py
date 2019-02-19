e905e4444a924d3b2f4ae1359c2b41699082fe4f
makio135 <radisson@MAC22219-6.local>
2019-02-19 17:41:24 +0100

    0.3 tick for LCD

--- aipod.py
+++ aipod.py
@@ -49,7 +49,7 @@ music = pygame.mixer.music
 
 lcd_line_1 = ' ' * 16
 lcd_line_2 = ' ' * 16
-tick_delay = 0.4
+tick_delay = 0.3
 last_tick = start_time = time.time()
 
 
@@ -150,7 +150,7 @@ while True:
         index = int(elapsed_time / tick_delay) % len(lcd_line_1)
         line_1 = (lcd_line_1 * 2)[index:min(index + 16, len(lcd_line_1 * 2))]
         line_2 = (lcd_line_2 * 2)[index:min(index + 16, len(lcd_line_2 * 2))]
-        print(line_1 + '\n' + line_2)
+        # print(line_1 + '\n' + line_2)
         lcd.message = line_1 + '\n' + line_2
         last_tick = time.time()
 
