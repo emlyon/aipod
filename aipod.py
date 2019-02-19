94eebc77f448d127e8118c61bb36cfac29d36e3a
makio135 <radisson@MAC22219-6.local>
2019-02-19 15:56:12 +0100

    refactor

--- aipod.py
+++ aipod.py
@@ -59,27 +59,20 @@ def map_value(value, istart, istop, ostart, ostop):
 do_fade_in = False
 def fade_in(start, duration):
     global music, time, do_fade_in
-
     v = map_value(time.time(), start, start + duration, 0, 100)/100
-    print('fade_in v:' + str(v) + '     v >= 1: ' + str(v >= 1))
     music.set_volume(v)
-
     if v >= 1:
-        print('end fade in')
         do_fade_in = False
 
 do_fade_out = False
 def fade_out(start, duration):
-    global music, time, lcd, do_fade_out
-
+    global music, time, lcd, do_fade_out, lcd_line_1, lcd_line_2
     v = map_value(time.time(), start, start + duration, 100, 0)/100
-    print('fade_out v:' + str(v) + '     v <= 0: ' + str(v <= 0))
     music.set_volume(v)
-
     if v <= 0:
-        print('end fade out')
         music.stop()
-        lcd.message = (' ' * 16) + "\n" + (' ' * 16)
+        lcd_line_1 = ' ' * 16
+        lcd_line_2 = ' ' * 16
         do_fade_out = False
 
 def play_song():
@@ -118,7 +111,6 @@ while True:
 
     if button.is_pressed:
         do_release = False
-
         if button.previous_state == 'UP': # on button press
             print('button pressed')
             button.previous_state = 'DOWN'
@@ -126,7 +118,6 @@ while True:
             time.sleep(.5)
             start_time = time.time()
             elapsed_time = 0
-
     else:
         if button.previous_state == 'DOWN' and not do_release: # on button release
             print('button released')
@@ -140,22 +131,13 @@ while True:
                 print('starting fade out')
                 do_fade_out = True
                 start_fade_time = now
-
-            button.previous_state = 'UP'
+                button.previous_state = 'UP'
 
     if music.get_busy() == 1:
-        # update lcd message
-        index = int(elapsed_time / tick_delay) % len(lcd_line_1)
-        line_1 = (lcd_line_1 * 2)[index:min(index + 16, len(lcd_line_1 * 2))]
-        line_2 = (lcd_line_2 * 2)[index:min(index + 16, len(lcd_line_2 * 2))]
-        # lcd.message = line_1 + '\n' + line_2
-        lcd.message = 'playing         ' + '\n' + (' ' * 16)
-
         if do_fade_out:
             fade_out(start_fade_time, 5)
         elif do_fade_in:
             fade_in(start_fade_time, 5)
-
     elif button.is_pressed:
         # play a new song
         play_song()
@@ -163,4 +145,11 @@ while True:
         start_time = time.time()
         elapsed_time = 0
 
-    time.sleep(0.05) # small delay
+
+    # update lcd message
+    index = int(elapsed_time / tick_delay) % len(lcd_line_1)
+    line_1 = (lcd_line_1 * 2)[index:min(index + 16, len(lcd_line_1 * 2))]
+    line_2 = (lcd_line_2 * 2)[index:min(index + 16, len(lcd_line_2 * 2))]
+    lcd.message = line_1 + '\n' + line_2
+
+    time.sleep(0.1) # small delay
